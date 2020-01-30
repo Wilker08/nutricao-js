4834dcf532ab70ed462e1627c27eae786458a069
Roberto <robertowilker08@gmail.com>
2020-01-30 13:48:59 -0300

    Calculo inicial do IMC.

--- nutricao-js/js/principal.js
+++ nutricao-js/js/principal.js
@@ -9,6 +9,13 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-console.log(paciente.textContent);
-console.log(peso);
-console.log(altura);
+var tdImc = paciente.querySelector(".info-imc");
+var imc = peso / (altura * altura); // 100 / 2.0 x 2.0 = 100 / 4 =>>>> 25
+
+tdImc.textContent = imc;
+
+console.log(tdImc.textContent);
+
+//console.log(paciente.textContent);
+//console.log(peso);
+//console.log(altura);

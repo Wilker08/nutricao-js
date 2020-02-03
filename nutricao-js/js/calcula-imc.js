4d02218c1e672183d1a87c5d103e9167e34fa857
Roberto <robertowilker08@gmail.com>
2020-02-03 13:16:49 -0300

    Arquivo principal.js foi dividido e renomeado para calcula-imc, para que assim fique mais semantico.

---
+++ nutricao-js/js/calcula-imc.js
@@ -0,0 +1,53 @@
+var tituloDoIndex = document.querySelector("h1");
+tituloDoIndex.textContent = "Aparecida Nutricionista";
+
+tituloDoIndex.addEventListener("click", mostraMensagem);
+function mostraMensagem(){
+    console.log("Ola, eu fui clicado :D");
+}
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+    
+    console.log(pacientes[i]);
+    var paciente = pacientes[i];
+
+    var tdNome = paciente.querySelector(".info-nome");
+    var nome = tdNome.textContent;
+
+    var tdPeso =  paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    // validacao
+    var pesoEhValido = true;
+    var alturaEhValido = true;
+
+    if(peso <= 0 || peso >= 300){
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    if(altura <= 0.0 || altura >= 3.00){
+        console.log("Altura inválido");
+        alturaEhValido = false;
+        tdImc.textContent = "Altura inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValido){
+        var imc = peso / (altura * altura); // 100 / 2.0 x 2.0 = 100 / 4 =>>>> 25
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+console.log(tdImc.textContent);
+
+//console.log(paciente.textContent);
+//console.log(peso);
+//console.log(altura);

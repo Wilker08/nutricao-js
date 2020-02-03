0f7d1d323702b4a5a7f517088bb915ff2178171d
Roberto <robertowilker08@gmail.com>
2020-02-03 13:18:04 -0300

    Arquivo form.js contem toda a logica para cadastro e adição de um novo paciente a tabela-pacientes.

---
+++ nutricao-js/js/form.js
@@ -0,0 +1,36 @@
+var botaoAdicionar  = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    // captura as informações do formulario para criar um novo paciente
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    // cria tr
+    var pacienteTr = document.createElement("tr");
+
+    // cria td
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    // adicionando os dados capturados do form-adiciona
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    // adicionando os td's como filhos de tr
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
